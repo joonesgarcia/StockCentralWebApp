0cd72e20b25bcba2f6659e500332b6756c2da6be
João V Garcia <joao.garcia.vitor@hotmail.com>
2023-04-22 17:49:42 -0300

    Loads data from api to principalGrid

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainGridComponent } from './main-grid/main-grid.component';
+import { GuiGridModule } from '@generic-ui/ngx-grid';
+import { HttpClientModule } from '@angular/common/http';
+
 
 @NgModule({
   declarations: [
@@ -12,7 +15,9 @@ import { MainGridComponent } from './main-grid/main-grid.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    GuiGridModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
